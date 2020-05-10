0967e5d1b81e3d3346703e96efc1ef9d484d53ed
pouyajabbarisani <pouyajabbarisani@gmail.com>
2020-05-10 11:10:30 +0300

    button and input components added

---
+++ components/Button.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+
+   const loadingLoader = () => {
+      return <div className="button-loader">
+         <div className="button-loader-bullet"></div>
+         <div className="button-loader-bullet"></div>
+         <div className="button-loader-bullet"></div>
+         <style jsx>{`
+            .button-loader-bullet{
+               width: 0.7rem;
+               height: 0.7rem;
+               display: inline-block;
+               margin: 0 0.2rem;
+               background: #fff;
+               border-radius: 1rem;
+            }
+            @keyframes sides {
+               0%   {transform: translateY(-0.1rem)}
+               50% {transform: translateY(0.1rem)}
+               100%  {transform: translateY(-0.1rem)}
+            }
+            @keyframes centeral {
+               0%   {transform: translateY(0.1rem)}
+               50% {transform: translateY(-0.1rem)}
+               100%   {transform: translateY(0.1rem)}
+            }
+
+            .button-loader-bullet:nth-of-type(1),
+            .button-loader-bullet:nth-of-type(3){
+               animation-name: sides;
+               animation-duration: 0.6s;
+               animation-timing-function: linear;
+               animation-iteration-count: infinite;
+            }
+            .button-loader-bullet:nth-of-type(2){
+               animation-name: centeral;
+               animation-duration: 0.6s;
+               animation-timing-function: linear;
+               animation-iteration-count: infinite;
+            }
+         `}</style>
+      </div>
+   }
+
+   const classLists = () => {
+      let listString = ''
+      props.isloading && (listString += ' disabled-button ');
+      props.isFullWidth && (listString += ' fullwidth-button ');
+      return listString
+   }
+
+   return <button onClick={e => !(!!props.isloading) && props.onClick(e)} className={classLists()}> {props.isloading ? loadingLoader() : props.label}</button >
+}
+export default Button
--- components/Input.js
+++ components/Input.js
@@ -1,4 +1,33 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+
 const Input = (props) => {
+
+   const [dynamicProps, setDynamicProps] = useState({})
+   const [errorMessage, setErrorMessage] = useState('')
+
+   function onChangeCustomiser(e) {
+      // if (props.type == 'username') {
+      //    e.target.value = e.target.value.replace(/[^A-Z0-9]/ig, "");
+      // }
+      props.onChange(e);
+   }
+
+   function onBlurCustomizer(e) {
+      // if (props.type == 'password' && e.target.value.length < 6) {
+      //    setErrorMessage('Password length should be at lest 6')
+      //    props.haveError(true)
+      // }
+      // else if (props.type == 'password' && !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,30}$/.test(e.target.value)) {
+      //    setErrorMessage('Password should have letter and number with length from 6 to 30')
+      //    props.haveError(true)
+      // }
+      // else {
+      setErrorMessage('')
+      props.haveError(false)
+      // }
+   }
+
    function labelHandler() {
       if (props.label) {
          return <label>{props.label}</label>
@@ -8,7 +37,9 @@ const Input = (props) => {
    return (
       <div className="input-container">
          {labelHandler()}
-         <input type={props.type || 'text'} placeholder={props.placeholder || ''} />
+         <input name={props.type || ''} type={props.type || 'text'} placeholder={props.placeholder || ''} onChange={e => onChangeCustomiser(e)} onBlur={e => onBlurCustomizer(e)} {...dynamicProps} className={errorMessage && errorMessage != '' ? 'input-on-error' : ''} />
+
+         {errorMessage && errorMessage != '' ? <span className="input-error">{errorMessage}</span> : ''}
       </div>
    )
 }
