12237f1acf98566c0e8debd015fda049bcd8bb3c
pouyajabbarisani <pouyajabbarisani@gmail.com>
2020-06-19 21:22:52 +0300

    input test suites added.

---
+++ components/__tests__/Input.test.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Input from '../Input'
+
+describe("Input Tests", () => {
+   const placeholderText = 'testinput'
+
+   test("Check input default value", () => {
+      const { getByPlaceholderText } = render(<Input placeholder={placeholderText} value="test default" />)
+      const input = getByPlaceholderText(placeholderText)
+      expect(input.value).toEqual('test default')
+   })
+
+   test("Check onChange method have been called currectly", () => {
+      const testChangeMethod = jest.fn();
+      const { getByPlaceholderText } = render(<Input placeholder={placeholderText} onChange={e => testChangeMethod(e)} />)
+      const input = getByPlaceholderText(placeholderText)
+      expect(testChangeMethod).not.toHaveBeenCalled();
+
+      fireEvent.change(input, { target: { value: "new input content" } })
+      expect(testChangeMethod).toHaveBeenCalled();
+   })
+
+   test("Check onBlur method have been called currently", () => {
+      const testBlurMethod = jest.fn();
+      const { getByPlaceholderText } = render(<Input placeholder={placeholderText} onBlur={e => testBlurMethod(e)} />)
+      const input = getByPlaceholderText(placeholderText)
+      fireEvent.focus(input)
+      expect(testBlurMethod).not.toHaveBeenCalled();
+
+      fireEvent.blur(input)
+      expect(testBlurMethod).toHaveBeenCalled();
+   })
+
+   test("Check label rendered currectly", () => {
+      const sampleLabelForTest = 'sample label';
+      const { queryByText } = render(<Input placeholder={placeholderText} label={sampleLabelForTest} />)
+      expect(queryByText(sampleLabelForTest)).toBeTruthy()
+   })
+
+
+})
